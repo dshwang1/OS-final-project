7e9f485f5794c40327aa5d20b2ed527d14c70b9a
Daniel Hwang <32149999+taemoon115@users.noreply.github.com>
2018-11-27 17:41:08 -0500

    updated

--- osFinal/src/osFinal/Final.java
+++ osFinal/src/osFinal/Final.java
@@ -2,19 +2,49 @@ package osFinal;
 
 //import com.wanikani.api.config.Configuration;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.io.*;
 import java.util.Scanner;
 
+
 public class Final {
 
-	public static void main(String[] args) throws MalformedURLException{
+	public static void main(String[] args) throws MalformedURLException,IOException{
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		System.out.println("Type your wanikani api here: ");
 		String key = input.nextLine();
 		
-		URL url = new URL("https://www.wanikani.com/api/user/" + key + "/kanji?callback=define");
-
+		String url = "https://www.wanikani.com/api/user/" + key + "/kanji?callback=define";
+		URL obj = new URL(url);
+		
+		//Here request and reading response codes
+		//are taken from https://www.baeldung.com/java-http-request
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		
+		//con.setConnectTimeout(5000);
+		//con.setReadTimeout(5000);
+		
+		int status = con.getResponseCode();
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while((inputLine = in.readLine()) != null)
+		{
+			content.append(inputLine);
+		}
+		in.close();
+		con.disconnect();
+		//end of referenced code. 
+		System.out.println(content);
+		String example = content.toString();
+		
+		FileWriter out = new FileWriter("output.txt");
+		out.write(example);
+		out.close();
+		
 	}
 
 }
